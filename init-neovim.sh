33e1071dc6e338d4fa6ba081d1d52d38e83a7d1c
michalchochol <michal.chochol@gmail.com>
2022-07-13 22:31:43 +0200

    Update init-neovim.sh

--- init-neovim.sh
+++ init-neovim.sh
@@ -3,10 +3,12 @@ sudo apt-get -y update
 sudo apt-get -y install neovim
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
-unzip Hack.zip
 mkdir ~/.local/share/fonts
 mkdir ~/.local/share/fonts/hack
-sudo mv ./ttf/* ~/.local/share/fonts/hack
+unzip -d ~/.local/share/fonts/hack Hack.zip 
+
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim/site ~/.local/share/nvim/site.bak
 
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 nvim +PackerSync
