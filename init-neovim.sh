ff81f6ff3f98bfe4073aae86dc248de16c6de81b
michalchochol <michal.chochol@gmail.com>
2022-07-13 22:37:45 +0200

    Update init-neovim.sh

--- init-neovim.sh
+++ init-neovim.sh
@@ -1,6 +1,6 @@
-#sudo add-apt-repository -y ppa:neovim-ppa/unstable
-#sudo apt-get -y update
-#sudo apt-get -y install neovim
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get -y update
+sudo apt-get -y install neovim
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
 rm -rf ~/.local/share/fonts/hack
@@ -8,8 +8,8 @@ mkdir ~/.local/share/fonts
 mkdir ~/.local/share/fonts/hack
 unzip -d ~/.local/share/fonts/hack Hack.zip 
 
-#mv ~/.config/nvim ~/.config/nvim.bak
-#mv ~/.local/share/nvim/site ~/.local/share/nvim/site.bak
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim/site ~/.local/share/nvim/site.bak
 
-#git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-#nvim +PackerSync
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
