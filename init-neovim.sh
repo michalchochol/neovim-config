28f5e5598e123f360a1d29c74c205ca0418416c5
michalchochol <michal.chochol@gmail.com>
2022-07-13 21:56:54 +0200

    Update init-neovim.sh

--- init-neovim.sh
+++ init-neovim.sh
@@ -1,6 +1,9 @@
-brew install neovim
 
-brew tap homebrew/cask-fonts
-brew install --cask font-hack-nerd-font
+sudo apt-get install neovim
+
+wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
+unzip Hack-v3.003-ttf.zip
+sudo mv ./ttf/* ~/.local/share/fonts/hack
 
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
