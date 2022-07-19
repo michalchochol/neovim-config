d9fce73388fc743fd63540e933987aea8fff0bf1
michalchochol <michal.chochol@gmail.com>
2022-07-19 13:03:51 +0200

    Update init-neovim.sh

--- init-neovim.sh
+++ init-neovim.sh
@@ -3,6 +3,7 @@ brew install neovim
 brew tap homebrew/cask-fonts
 brew install ripgrep
 brew install jesseduffield/lazygit/lazygit
+brew install jesseduffield/lazydocker/lazydocker
 brew install --cask font-hack-nerd-font
 
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
