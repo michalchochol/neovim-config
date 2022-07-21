ba5249202246b711c263d508314475bdab89f2fc
Michał Chochół <michal.chochol@ge.com>
2022-07-21 09:37:45 +0200

    mapping modified

--- user/init.lua
+++ user/init.lua
@@ -225,15 +225,15 @@
       ["<S-A-Right>"] = { "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer tab right" },
       ["<S-A-Left>"] = { "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer tab left" },
       -- Better window navigation
-      ["<C-S-Left>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
-      ["<C-S-Down>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
-      ["<C-S-Up>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
-      ["<C-S-Right>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
+      ["<S-Left>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
+      ["<S-Down>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
+      ["<S-Up>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
+      ["<S-Right>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
       -- Resize with arrows
-      ["<S-Up>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" },
-      ["<S-Down>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" },
-      ["<S-Left>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" },
-      ["<S-Right>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
+      ["<C-Up>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" },
+      ["<C-Down>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" },
+      ["<C-Left>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" },
+      ["<C-Right>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
     },
     t = {
       -- setting a mapping to false will disable it
