a778f41a10b3de69bf7f6bfaee6e5f147e526560
Michał Chochół <michal.chochol@ge.com>
2022-10-05 13:42:41 +0200

    update

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,6 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
+-- lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
