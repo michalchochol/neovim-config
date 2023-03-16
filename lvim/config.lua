afb00befa4276567a7e14e87a0d2ef356e89ca46
Michał Chochół <michal.chochol@ge.com>
2023-03-16 11:34:57 +0100

    golang debugger configuration added

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,7 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
--- lvim.builtin.dap.active = true
+lvim.builtin.dap.active = true
 -- lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -22,10 +22,10 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- buffers
-lvim.keys.normal_mode["<A-Right>"] = "<cmd>BufferLineCycleNext<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-lvim.keys.normal_mode["<A-Left>"] = "<cmd>BufferLineCyclePrev<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+lvim.keys.normal_mode["<A-Right>"] = "<cmd>BufferLineCycleNext<cr>"  -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+lvim.keys.normal_mode["<A-Left>"] = "<cmd>BufferLineCyclePrev<cr>"   -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 lvim.keys.normal_mode["<S-A-Right>"] = "<cmd>BufferLineMoveNext<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-lvim.keys.normal_mode["<S-A-Left>"] = "<cmd>BufferLineMovePrev<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+lvim.keys.normal_mode["<S-A-Left>"] = "<cmd>BufferLineMovePrev<cr>"  -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 -- Better window navigation
 lvim.keys.normal_mode["<C-v>"] = ":vsplit<CR>"
 lvim.keys.normal_mode["<C-h>"] = ":split<CR>"
@@ -41,6 +41,7 @@ lvim.builtin.which_key.mappings["t"] = {
   h = { "<cmd>ToggleTerm direction=horizontal size=20<cr>", "horizontal" },
   v = { "<cmd>ToggleTerm direction=vertical size=80<cr>", "vertical" },
 }
+lvim.keys.normal_mode["<leader>B"] = ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
@@ -79,7 +80,7 @@ lvim.builtin.which_key.mappings["t"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
+-- lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
@@ -147,8 +148,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "shfmt", filetypes = { "sh" } },
+  { command = "black",        filetypes = { "python" } },
+  { command = "shfmt",        filetypes = { "sh" } },
   { command = "clang-format", filetypes = { "java" } },
   -- { command = "uncrustify", filetypes = { "groovy" } },
   -- { command = "npm-groovy-lint", filetypes = { "groovy" } },
@@ -188,6 +189,10 @@ lvim.plugins = {
   -- { "sbdchd/neoformat" },
   -- { "akinsho/toggleterm.nvim" },
   { "ravenxrz/DAPInstall.nvim" },
+  { "theHamsta/nvim-dap-virtual-text" },
+  { "nvim-telescope/telescope-dap.nvim" },
+  -- { "leoluz/nvim-dap-go" },
+  -- { "ray-x/go.nvim" }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -203,3 +208,66 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+--
+--
+
+
+
+-- Golang debugging configuration
+local dap = require("dap")
+
+dap.adapters.go = function(callback, _)
+  local stdout = vim.loop.new_pipe(false)
+  local handle
+  local pid_or_err
+  local port = 38697
+  local opts = {
+    stdio = { nil, stdout },
+    args = { "dap", "-l", "127.0.0.1:" .. port },
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+    stdout:close()
+    handle:close()
+    if code ~= 0 then
+      print("dlv exited with code", code)
+    end
+  end)
+  assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+  -- Wait for delve to start
+  vim.defer_fn(function()
+    callback { type = "server", host = "127.0.0.1", port = port }
+  end, 100)
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  {
+    type = "go",
+    name = "Debug test",   -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}",
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "go",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}",
+  },
+}
